aba17bd26172057cad3e179824e3e5f9844ade3a
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2020-09-18 23:32:44 +0700

    add service worker

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./index.scss";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.register()
