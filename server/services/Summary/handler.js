b4b24320fe67bf11fa111687b463f330bf7252d2
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2020-11-20 18:08:23 +0700

    WIP: In out sales

--- server/services/Summary/handler.js
+++ server/services/Summary/handler.js
@@ -1,27 +1,36 @@
 const db = require('../../db')
 const { RESELLER, AGEN } = require('../../constants/roles')
+const { json } = require('express')
+const { select } = require('../../db')
 
 module.exports = {
-    getTop10Reseller: async (req, res, next) => {
-        const { range = 1 } = req.query
+    getInOut: async (req, res, next) => {
         try {
-            const data =
-                await db('penjualan as p')
-                    .select('p.seller_id', 'b.nama', db.raw(`sum(p.jumlah*pr.reseller_price) as total`))
-                    .join('prices as pr', 'pr.product_id', '=', 'p.product_id')
+            const income =
+                db('penjualan as p')
+                    .select('p.waktu', 'p3.product_name', 'p.jumlah', db.raw('p.jumlah*p2.admin_price as harga'), db.raw('true as is_income'))
+                    .join('prices as p2', 'p.product_id', 'p2.product_id')
+                    .join('products as p3', 'p3.id', '=', 'p.product_id')
                     .join('users as u', 'u.id', '=', 'p.seller_id')
-                    .join('profiles as b', 'b.user_id', '=', 'u.id')
-                    .where('u.role_id', RESELLER.id)
-                    .whereRaw(`cast(p.waktu as date) <= CURDATE() and cast(p.waktu as date) > CURDATE() - ${range}`)
-                    .orderBy('total', 'desc')
-                    .groupBy('p.seller_id')
-                    .limit(10)
-            res.status(200).json(data)
+                    .join('roles as r2', 'u.role_id', '=', 'r2.id')
+                    .whereRaw(`MONTH (waktu) = MONTH(CURDATE()) and r2.role_name='Admin'`)
+                    .as('income')
+            const outcome =
+                db('pembelian as p')
+                    .select('p.waktu', 'p3.product_name', 'p.jumlah', db.raw('p.jumlah*p2.buy_price as harga'), db.raw('false as is_income'))
+                    .join('prices as p2', 'p.product_id', '=', 'p2.product_id')
+                    .join('products as p3', 'p3.id', '=', 'p.product_id')
+                    .whereRaw('MONTH (waktu) = MONTH(CURDATE())')
+                
+            const builder = db.select('*').from(income.union(outcome))
+
+            const totalIncome = await builder.clone()
+
+            res.json(totalIncome)
         } catch (err) {
             next(err)
         }
     },
-
     getTop10Agen: async (req, res, next) => {
         const { range = 1 } = req.query
         try {
@@ -41,17 +50,6 @@ module.exports = {
             next(err)
         }
     },
-
-    getTop10Region: async (req, res, next) => {
-        const { range = 1 } = req.query
-        try {
-            const regions = await db('regions')
-            res.status(200).json(data)
-        } catch (err) {
-            next(err)
-        }
-    },
-
     getSummarizedSalesData: async (req, res, next) => {
 
     },
--- server/services/Summary/index.js
+++ server/services/Summary/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const __ = require('../../middlewares')
 const _ = require('./handler')
 
-router.get('/top10reseller', __.checkToken, _.getTop10Reseller)
+router.get('/inoutproducts', __.checkToken, _.getInOut)
 // router.put('/', __.checkToken, _.editRegion)
 // router.post('/', __.checkToken, _.addRegion)
 // router.delete('/', __.checkToken, _.deleteRegion)
