258f9a02830b3e4272cc690305ac4709ad2f753d
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2020-11-23 23:40:11 +0700

    revisi docker compose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const services = require('./services')
 require('./db')
