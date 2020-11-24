4a022f9e897c4049f434ad7e5b1b9f704be47f29
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2020-11-24 15:20:20 +0700

    remove print env

--- server/index.js
+++ server/index.js
@@ -13,5 +13,4 @@ app.use(express.json({ type: 'application/vnd.api+json' }))
 app.use(express.urlencoded({ extended: false }))
 app.use('/', services)
 
-console.log(process.env)
 app.listen(PORT, () => console.log('Server run at port', PORT))
