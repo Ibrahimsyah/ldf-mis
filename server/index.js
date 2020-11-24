56f7170fe8a5837dfda08cede22a50f41956b064
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2020-11-24 09:18:19 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,4 +13,5 @@ app.use(express.json({ type: 'application/vnd.api+json' }))
 app.use(express.urlencoded({ extended: false }))
 app.use('/', services)
 
+console.log(process.env)
 app.listen(PORT, () => console.log('Server run at port', PORT))
